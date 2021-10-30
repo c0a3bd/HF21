36c9cce2dbf08593871c4d86fb1e6aed1aec5096
c0a3bd <40324520+c0a3bd@users.noreply.github.com>
2021-10-30 14:02:32 +0000

    added first condition

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # https://www.youtube.com/watch?v=_ZqAVck-WeM
 
 name = input("What is your name? ")
-age = input("What is your age? ")
+age = int(input("What is your age? "))
 
 print("Hello", name, "you are", age, "years old.")
 
+if age >= int(4):
+  print("ok, you're old enough to play")
