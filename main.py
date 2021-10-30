308b696ef3af3884ba3d67c06134760c882d6aa0
c0a3bd <40324520+c0a3bd@users.noreply.github.com>
2021-10-30 13:54:42 +0000

    added references and game framework

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("hello, world!")
-print("hello, 2")
+# reference tutorial
+# Python Project Tutorial - Your First Python Project
+# https://www.youtube.com/watch?v=_ZqAVck-WeM
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+print("Hello", name, "you are", age, "years old.")
+
