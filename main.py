dccea946514e497f46b82c1656aad4beb83932cf
c0a3bd <40324520+c0a3bd@users.noreply.github.com>
2021-10-30 15:14:45 +0000

    keeping playing screen clean

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 ## 
 ## Replit - https://replit.com/
 ## Replit Docs - https://bit.ly/3pV67cu
-## Reference Tutorial - https://bit.ly/3ErPnOb
 #####################################################
+
+import replit
 
 name = input("What is your name? ")
 age = int(input("What is your age? "))
@@ -15,9 +16,14 @@ print("Hello", name, "you are", age, "years old.")
 
 old_enough_to_play = int(4)
 if age >= old_enough_to_play:
+  replit.clear()
   print("ok, you're old enough to play")
+
+
 elif age < 0:
+    replit.clear()
     print("ok, it's Hacktoberfest so zombies can play too!")
 else:
-  print("come back in ", old_enough_to_play - age, "years.")
+    replit.clear()
+    print("come back in ", old_enough_to_play - age, "years.")
   
